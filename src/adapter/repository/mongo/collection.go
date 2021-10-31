9d4671b6580490787529f1007ebcde747e9e1e72
Vagner Gonçalves <vlgonalves@gmail.com>
2021-10-31 19:40:27 -0300

    feat(inicial): first commit include mongodb and domain item supermarket list

---
+++ src/adapter/repository/mongo/collection.go
@@ -0,0 +1,34 @@
+package mongo
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var ctx = context.TODO()
+
+func collection() *mongo.Collection {
+
+	clientOptions := options.Client().ApplyURI("mongodb://mongo:27017/").SetAuth(
+		options.Credential{
+			Username: "root",
+			Password: "123456",
+		},
+	)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+		panic("Erro ao conectar com o banco de dados")
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+		panic("Erro ao conectar com o banco de dados")
+	}
+
+	return client.Database("supermaketlist").Collection("list")
+}
